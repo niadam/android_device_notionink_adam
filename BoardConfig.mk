f3ffdb5afc3401824c0c25eba8642c54706cd186
Boris Popov <bvpopov@gmail.com>
2012-08-13 12:03:20 -0700

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+
+#  Use the Adam common BoardConfig
+include device/notionink/adam_common/BoardConfigCommon.mk
+
+TARGET_NO_BOOTLOADER := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_ARCH_VARIANT_FPU := vfpv3-d16
+TARGET_CPU_SMP := true
+#TARGET_HAVE_TEGRA_ERRATA_657451 := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# 3G
+#BOARD_MOBILEDATA_INTERFACE_NAME := "ppp0"
---
+++ cm.mk
@@ -0,0 +1,53 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Camera
+PRODUCT_PACKAGES := \
+    Camera \
+    SpareParts \
+    PQiToggle \
+    Development #\
+    #libhuaweigeneric-ril \
+    #Stk \
+    #Mms
+
+DEVICE_PACKAGE_OVERLAYS += device/notionink/adam/overlay
+
+# Some files for external 3G
+#PRODUCT_COPY_FILES += \
+#    device/notionink/adam/files/ppp/ip-up:/system/etc/ppp/ip-up \
+#    device/notionink/adam/files/ppp/ip-down:/system/etc/ppp/ip-down \
+#    device/notionink/adam/files/wwlan_select.sh:/system/bin/wwlan_select.sh \
+#    device/notionink/adam/files/rild:/system/bin/rild
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from ADAM device common
+$(call inherit-product, device/notionink/adam_common/device-common.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/notionink/adam/device-vendor.mk)
+
+PRODUCT_NAME := cm_adam
+PRODUCT_DEVICE := adam
+PRODUCT_BRAND := NotionInk
+PRODUCT_MODEL := Notion Ink ADAM
+
+$(call inherit-product, device/notionink/adam_common/google_apps.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
