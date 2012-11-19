14ecec0268b61b8c55029fc45cf4cc8f8c72a65b
Boris Popov <bvpopov@gmail.com>
2012-11-19 00:04:44 -0800

    Remove auto-patch apply Apply patches manually and only once

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,18 +21,18 @@
 # function: add_lunch_combo generic-eng
 export USE_CCACHE=1
 add_lunch_combo full_adam-userdebug
-echo ""
-echo "Patching Adam Workspace..."
-echo ""
-for p in $(find device/notionink/adam/patches/ -name "*.diff") 
-	do 
-		echo -n "Apply patch "$(basename $p | awk -F"." '{print $1}')
-		patch -p1 < $p > /dev/null 2>&1
-		if [ $? == 0 ]; then
-			echo "     [DONE]"
-		else
-			echo "     [FAIL]"
-		fi
-		echo "" 
-	done
-echo ""
+#echo ""
+#echo "Patching Adam Workspace..."
+#echo ""
+#for p in $(find device/notionink/adam/patches/ -name "*.diff") 
+#	do 
+#		echo -n "Apply patch "$(basename $p | awk -F"." '{print $1}')
+#		patch -p1 < $p > /dev/null 2>&1
+#		if [ $? == 0 ]; then
+#			echo "     [DONE]"
+#		else
+#			echo "     [FAIL]"
+#		fi
+#		echo "" 
+#	done
+#echo ""
