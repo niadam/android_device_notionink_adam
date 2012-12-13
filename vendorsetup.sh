577f6b8448e3ba590c6a895d13bee66b9b57f7ef
Boris Popov <bvpopov@gmail.com>
2012-12-13 05:06:37 -0800

    Improve patch script

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -26,13 +26,21 @@ echo "Patching Adam Workspace..."
 echo ""
 for p in $(find device/notionink/adam/patches/ -name "*.diff") 
 	do 
-		echo -n "Apply patch "$(basename $p | awk -F"." '{print $1}')
-		patch -p1 < $p > /dev/null 2>&1
-		if [ $? == 0 ]; then
-			echo "     [DONE]"
+		tmp=$(basename $p | awk -F"." '{print $1}')
+		if [ -f $tmp".p" ]; then
+			echo "Patch "$tmp" already applied"
 		else
-			echo "     [FAIL]"
+			echo -n "Apply patch "$tmp
+			patch -p1 < $p > /dev/null 2>&1
+			if [ $? == 0 ]; then
+				echo "     [DONE]"
+				touch $tmp".p"
+			else
+				echo "     [FAIL]"
+			fi
 		fi
 		echo "" 
 	done
+echo "Cleaning .orig and .rej files if any..."
+find . \( -name \*cpp.orig -o -name \*xml.orig -o -name \*.java.orig -o -name \*.h.orig -o -name \*.rej \) -delete
 echo ""
